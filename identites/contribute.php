c8f95e5297ac2e7c04ca9cd3a30d5d350991e37c
Tristan Rivoallan <tristan@rivoallan.net>
2011-12-03 10:16:03 +0100

    Added google analytics tracking code.

--- identites/contribute.php
+++ identites/contribute.php
@@ -81,7 +81,19 @@ if (isset($_FILES['file'])) {
 <?php if($feedback): ?>
 		<p class="<?php echo $feedback['class'] ?>"><strong><?php echo $feedback['text'] ?></strong></p>
 <?php endif; ?>
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27467726-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 	</body>
 
 </html>
